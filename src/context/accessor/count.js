d14d20d84e80d8a8f8de139330fce4c862893f91
Mihai Lazar <mihai.lazar@evozon.com>
2016-05-11 08:33:03 +0300

    Remove generated js files

--- src/context/accessor/count.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function count(cursor) {
-    return cursor.get('count');
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = count;
-//# sourceMappingURL=count.js.map
--- src/context/accessor/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var count_1 = require('./count');
-exports.count = count_1.default;
-//# sourceMappingURL=index.js.map
