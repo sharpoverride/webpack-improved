4453eb75e04c8f011d273b99f6f533e0f0ba2150
Mihai Lazar <mihai.lazar@evozon.com>
2016-06-14 14:33:18 +0300

    Provide simple links

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,19 @@
 import * as React from 'react';
+import {Link} from 'react-router';
 
 export default (props) => {
-    return (<div>{props.children}</div>);
+    return (<div>
+        <section>
+            <h1>Links</h1>
+            <ul>
+                <li>
+                    <Link to="/">Sample Counter</Link>
+                </li>
+                <li>
+                    <Link to="/todos/">Todos</Link>
+                </li>
+            </ul>
+        </section>
+        {props.children}
+    </div>);
 };
--- src/sample/entry-page.tsx
+++ src/sample/entry-page.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Link} from 'react-router';
 import CounterDisplay from './counter-display';
 import CounterIncrease from './counter-increase';
 
